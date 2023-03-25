ccc5e7ead37e6d3e11bde3b7390554defd268fe4
Andre Luca <lucaprojob@gmail.com>
2023-03-24 22:59:36 -0300

    appearence: set app header

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import logoToDo from './assets/logo.svg'
 
+function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <img
+          className="app-logo"
+          src={logoToDo} alt="Logo TO DO com um foguete azul."
+        />
+      </header>
+      <main>
+        <section className="task-creator">
+          <input type="text" />
+          <button>Criar</button>
+        </section>
+        <section className="task-list">
+          <div className="task-list-header">
+            <p>
+              Tarefas criadas
+              <span></span>
+            </p>
+            <p>
+              Concluídas
+              <span></span>
+            </p>
+          </div>
+          <ul className="task-container">
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, aut!</li>
+            <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab eveniet adipisci cumque saepe assumenda.</li>
+            <li>Lorem ipsum dolor sit amet consectetur.</li>
+          </ul>
+        </section>
+      </main>
     </div>
   )
 }
