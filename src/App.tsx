48dff54e489dbcf78e5725f80134327774065c6a
Andre Luca <lucaprojob@gmail.com>
2023-06-24 19:42:13 -0300

    add adding tasks functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react'
 import {
   PlusCircle,
   CheckCircle,
@@ -9,7 +10,28 @@ import clipboardImg from './assets/clipboard.png'
 import logoToDo from './assets/logo.svg'
 import './App.css'
 
+type Task = {
+  id: number,
+  done: boolean,
+  description: string
+}
+
 function App() {
+  const taskInputRef = useRef<HTMLInputElement>(null)
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  const handleAddTask = (): void => {
+    const taskInput = taskInputRef.current as HTMLInputElement
+
+    const newTask: Task = {
+      id: tasks.length + 1,
+      done: false,
+      description: taskInput.value
+    }
+
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="App">
       <header>
@@ -21,10 +43,12 @@ function App() {
       <main>
         <section className="task-creator">
           <input
+            ref={taskInputRef}
             placeholder="Adicione uma nova tarefa"
             type="text" />
           <button
             className="new-task-button"
+            onClick={handleAddTask}
           >
             Criar
             <PlusCircle size={16} />
@@ -47,43 +71,53 @@ function App() {
               <strong>Você ainda não tem tarefas cadastradas</strong> <br />
               Crie tarefas e organize seus itens a fazer
             </p> */}
-            <li className="task">
-              <div className="icon-container">
-                <div className="undone-icon-bg" />
-                <Circle
-                  className="undone-icon"
-                  size={24}
-                />
-              </div>
-              <p>
-                Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-              </p>
-              <div className="trash-icon-container">
-                <Trash
-                  className="trash-icon"
-                  size={24}
-                />
-              </div>
-            </li>
-            <li className="task done">
-              <div className="icon-container">
-                <div className="done-icon-bg" />
-                <CheckCircle
-                  className="done-icon"
-                  weight="fill"
-                  size={24}
-                />
-              </div>
-              <p>
-                Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-              </p>
-              <div className="trash-icon-container">
-                <Trash
-                  className="trash-icon"
-                  size={24}
-                />
-              </div>
-            </li>
+            {
+              tasks.map((task) => task.done ? (
+                <li
+                  key={task.id}
+                  className="task done"
+                >
+                  <div className="icon-container">
+                    <div className="done-icon-bg" />
+                    <CheckCircle
+                      className="done-icon"
+                      weight="fill"
+                      size={24}
+                    />
+                  </div>
+                  <p>
+                    {task.description}
+                  </p>
+                  <div className="trash-icon-container">
+                    <Trash
+                      className="trash-icon"
+                      size={24}
+                    />
+                  </div>
+                </li>
+              ) : (
+                <li
+                  key={task.id}
+                  className="task"
+                >
+                  <div className="icon-container">
+                    <div className="undone-icon-bg" />
+                    <Circle
+                      className="undone-icon"
+                      size={24}
+                    />
+                  </div>
+                  <p>
+                    {task.description}
+                  </p>
+                  <div className="trash-icon-container">
+                    <Trash
+                      className="trash-icon"
+                      size={24}
+                    />
+                  </div>
+                </li>))
+            }
           </ul>
         </section>
       </main>
